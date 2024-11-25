5b0c6af42368fcc6731b484dfaf2e14654f2fc45
Rafael Sabatini <bonieliasabatini@gmail.com>
2024-11-25 14:57:54 -0300

    Nova versão

--- darkMode.js
+++ darkMode.js
@@ -4,36 +4,11 @@ const darkSwitch = document.getElementById("darkSwitch");
 function enableDarkmode() {
   document.body.classList.add("darkMode");
   localStorage.setItem("darkMode", "active");
-
-  darkSwitch.classList.add("pushIn");
-
-  setTimeout( () => {
-    darkSwitch.classList.remove("pushIn");
-  }, 700);
- 
-  darkSwitch.style.borderStyle = "inset";
-  darkSwitch.style.borderColor = "var(--accent-color)";
-  darkSwitch.style.borderWidth = "2px";
 }
 
 function disableDarkmode() {
   document.body.classList.remove("darkMode");
   localStorage.setItem("darkMode", null);
-
-  darkSwitch.classList.add("pushOut");
-
-  setTimeout( () => {
-    darkSwitch.classList.remove("pushOut");
-  }, 700)
-
-  darkSwitch.style.borderStyle = "inset";
-  darkSwitch.style.borderColor = "var(--accent-color)";
-  darkSwitch.style.borderWidth = "2px";
-
-
-  darkSwitch.style.borderStyle = "outset";
-  darkSwitch.style.borderColor = "var(--accent-color)";
-  darkSwitch.style.borderWidth = "2px";  
 }
 
 if (darkMode === "active") enableDarkmode();
--- script.js
+++ script.js
@@ -1,108 +1,294 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     const problemCheckbox = document.getElementById("problemCheckbox");
     const doubtCheckbox = document.getElementById("doubtCheckbox");
+    const contactCheckbox = document.getElementById("contactCheckbox");
+    const humorSelection = document.getElementById("humorSelection")
     const upsellSituationYes = document.getElementById("upsellSituationYes");
     const upsellSituationNo = document.getElementById("upsellSituationNo");
     const errorPrintYes = document.getElementById("errorPrintYes");
     const errorPrintNo = document.getElementById("errorPrintNo");
+
+    const docNumber = document.getElementById("docNumber");
+    const errorMessage = document.getElementById("errorMessage");
+    const problemCause = document.getElementById("problemCause");
+    const resolution = document.getElementById("resolution");
+    const clientFeedback = document.getElementById("clientFeedback");
+    const upsellDescription = document.getElementById("upsellDescription");
+
+    const problemLabel = document.getElementById('problemLabel');
+    const resolutionLabel = document.getElementById('resolutionLabel');
+
     
-    problemCheckbox.addEventListener('change', function(){
-        if(this.checked) doubtCheckbox.checked = false;
+    // Array para armazenar os textos formatados
+    let textsToCopy = [];
+
+    resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+
+    problemCheckbox.addEventListener('change', function () {
+        if (this.checked) {
+            doubtCheckbox.checked = false;
+            contactCheckbox.checked = false;
+
+            docNumber.disabled = false;
+            errorMessage.disabled = false;
+            problemCause.disabled = false;
+
+            resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+            problemCause.placeholder = "Descreva o que ocasionou o erro ou situação"
+        }
     });
 
-    doubtCheckbox.addEventListener('change', function(){
-        if(this.checked) problemCheckbox.checked = false;
+    doubtCheckbox.addEventListener('change', function () {
+        if (this.checked) {
+            problemCheckbox.checked = false;
+            contactCheckbox.checked = false;
+
+            docNumber.disabled = true;
+            errorMessage.disabled = true;
+
+            problemLabel.innerHTML = "Dúvida do cliente"
+            resolutionLabel.innerHTML = "Explicação"
+
+            resolution.setAttribute("data-placeholder", "Descreva sua explicação para o cliente");
+            problemCause.placeholder = "Descreva a dúvida do cliente"
+        }
+        else{
+            document.getElementById('docNumber').disabled = false;
+            errorMessage.disabled = false;
+
+            
+            problemLabel.innerHTML = "Causa do Problema"
+            resolutionLabel.innerHTML = "Resolução"
+
+            resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+            problemCause.placeholder = "Descreva o que ocasionou o erro ou situação"
+        }
     });
 
-    errorPrintYes.addEventListener('change', function(){
-        if(this.checked) errorPrintNo.checked = false;
+    contactCheckbox.addEventListener('change', function () {
+        if (this.checked) {
+            doubtCheckbox.checked = false;
+            problemCheckbox.checked = false;
+
+            docNumber.disabled = true;
+            errorMessage.disabled = true;
+            problemCause.disabled = true;
+
+            resolutionLabel.innerHTML = "Relato do Contato"
+            resolution.setAttribute("data-placeholder", "Descreva o relato do contato com o cliente");
+        }
+        else{
+            docNumber.disabled = false;
+            errorMessage.disabled = false;
+            problemCause.disabled = false;
+
+            resolutionLabel.innerHTML = "Resolução"
+            resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+        }
+    });
+
+    errorPrintYes.addEventListener('change', function () {
+        if (this.checked) errorPrintNo.checked = false;
     })
 
-    errorPrintNo.addEventListener('change', function(){
-        if(this.checked) errorPrintYes.checked = false;
+    errorPrintNo.addEventListener('change', function () {
+        if (this.checked) errorPrintYes.checked = false;
     })
+
+    function addText(label, value) {
+        if (value) {
+            textsToCopy.push(`${label}: ${value}\n\n`).toString();
+        }
+    }
 
     document.getElementById('copyBtn').addEventListener('click', function (event) {
         event.preventDefault();
-    
-        // Array para armazenar os textos formatados
-        let textsToCopy = [];
 
-        if(!problemCheckbox.checked && !doubtCheckbox.checked){
-            createNotif("Selecione um tipo de chamado", "error");
-            return;
-        }
-        else if(!problemCause.value){
-            createNotif("Descreva a causa do problema", "error")
-            return;
-        }
-        else if(!resolution.innerText){
-            createNotif("Informe a resolução do suporte", "error")
-            return;
-        }
-        else if(humorSelection.value === 'Selecione'){
-            createNotif("Selecione o humor do cliente", "error")
-            return;
-        }
-        else if(!upsellSituationNo.checked && !upsellSituationYes.checked){
-            createNotif("Seleciona a situação do upsell", "error")
-            return;
-        }
-        else if(upsellSituationYes.checked && upsellDescription.value === ""){
-            createNotif("Descreva a situação do upsell", "error")
-            return;
-        }
-        else if(upsellSituationNo.checked && upsellDescription.value === ""){
-            createNotif("Descreva a situação do upsell", "error")
-            return;
-        }
-        else{
-            function addText(label, value) {
-                if (value) {
-                    textsToCopy.push(`${label}: ${value} \n\n`);
-                }
+        if(contactCheckbox.checked) {
+            if (!resolution.innerText) {
+                createNotif("Informe a resolução do suporte", "error")
+                return;
             }
-        
-            // Capturar os valores e adicionar ao array
-            addText('TIPO DE CHAMADO', document.querySelector('#doubtCheckbox').checked ? 'Dúvida' : document.querySelector('#problemCheckbox').checked ? 'Problema' : '');
-            addText('NÚMERO DO DOCUMENTO', document.getElementById('docNumber').value.trim());
-            addText('MENSAGEM DE ERRO', document.getElementById('errorMessage').value.trim());
-            addText('CAUSA DO PROBLEMA', document.getElementById('problemCause').value.trim());
-            addText('RESOLUÇÃO', document.getElementById('resolution').innerText.trim());
-            addText('FEEDBACK DO CLIENTE', document.getElementById('clientFeedback').value.trim());
-            addText('HUMOR DO CLIENTE', document.getElementById('humorSelection').value.trim());
+            else if (humorSelection.value === 'Selecione') {
+                createNotif("Selecione o humor do cliente", "error")
+                return;
+            }
+            else if (!upsellSituationNo.checked && !upsellSituationYes.checked) {
+                createNotif("Seleciona a situação do upsell", "error")
+                return;
+            }
+            else if (upsellSituationYes.checked && upsellDescription.value === "") {
+                createNotif("Descreva a situação do upsell", "error")
+                return;
+            }
+            else if (upsellSituationNo.checked && upsellDescription.value === "") {
+                createNotif("Descreva a situação do upsell", "error")
+                return;
+            }
+
+            addText('TIPO DE CHAMADO', 'Contato Ativo');
+            addText('RESOLUÇÃO', resolution.innerText);
+            addText('FEEDBACK DO CLIENTE', clientFeedback.value);
+            addText('HUMOR DO CLIENTE', humorSelection.value);
             addText('UPSELL', document.querySelector('#upsellSituationYes').checked ? 'Sim' : (document.querySelector('#upsellSituationNo').checked ? 'Não' : ''));
-            addText('DESCRIÇÃO UPSELL', document.getElementById('upsellDescription').innerText.trim());
+            addText('DESCRIÇÃO UPSELL', upsellDescription.innerText);
+
+            resolution.innerText = "";
+            clientFeedback.value = "";
+            humorSelection.value = "Selecione";
+            upsellDescription.innerText = "";
+    
+            let finalText = textsToCopy.join('');
+            navigator.clipboard.writeText(finalText);
+            createNotif("Texto Copiado!", "success");
+    
+            contactCheckbox.checked = false;
+            upsellSituationYes.checked = false;
+            upsellSituationNo.checked = false;
+            errorPrintYes.checked = false;
+            errorPrintNo.checked = false
+
+            docNumber.disabled = false;
+            errorMessage.disabled = false;
+            problemCause.disabled = false;
+
+            resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+        }
+        else if(doubtCheckbox.checked) {
+
+            if (!resolution.innerText) {
+                createNotif("Informe a resolução do suporte", "error")
+                return;
+            }
+            else if(problemCause.value == ""){
+                createNotif("Descreva a causa do problema", "error")
+                return;
+            }
+            else if (humorSelection.value === 'Selecione') {
+                createNotif("Selecione o humor do cliente", "error")
+                return;
+            }
+            else if (!upsellSituationNo.checked && !upsellSituationYes.checked) {
+                createNotif("Seleciona a situação do upsell", "error")
+                return;
+            }
+            else if (upsellSituationYes.checked && upsellDescription.value === "") {
+                createNotif("Descreva a situação do upsell", "error")
+                return;
+            }
+            else if (upsellSituationNo.checked && upsellDescription.value === "") {
+                createNotif("Descreva a situação do upsell", "error")
+                return;
+            }
+
+            addText('TIPO DE CHAMADO', 'Dúvida');
+            addText('CAUSA DO PROBLEMA', problemCause.value);
+            addText('RESOLUÇÃO', resolution.innerText);
+            addText('FEEDBACK DO CLIENTE', clientFeedback.value);
+            addText('HUMOR DO CLIENTE', humorSelection.value);
+            addText('UPSELL', document.querySelector('#upsellSituationYes').checked ? 'Sim' : (document.querySelector('#upsellSituationNo').checked ? 'Não' : ''));
+            addText('DESCRIÇÃO UPSELL', upsellDescription.innerText);
             addText('MENSAGENS OU PRINT DE ERROS', document.querySelector('#errorPrintYes').checked ? 'Sim' : (document.querySelector('#errorPrintNo').checked ? 'Não' : ''));
 
-            // Unir todos os textos em uma string com quebras de linha
             let finalText = textsToCopy.join('');
-            navigator.clipboard.writeText(finalText.trim());
+            navigator.clipboard.writeText(finalText);
             createNotif("Texto Copiado!", "success");
 
-            const docNumber = document.getElementById("docNumber").value = '';
-            const errorMessage = document.getElementById("errorMessage").value = '';
-            const problemCause = document.getElementById("problemCause").value = '';
-            const resolution = document.getElementById("resolution").innerText = '';
-            const clientFeedback = document.getElementById("clientFeedback").value = '';
-            const upsellDescription = document.getElementById("upsellDescription").innerText = '';
+            problemCause.value = '';
+            resolution.innerText = '';
+            clientFeedback.value = '';
+            upsellDescription.innerText = '';
 
-            problemCheckbox.checked = false;
+            humorSelection.value = 'Selecione';
+
             doubtCheckbox.checked = false;
             upsellSituationYes.checked = false;
             upsellSituationNo.checked = false;
             errorPrintYes.checked = false;
             errorPrintNo.checked = false;
 
+            docNumber.disabled = false;
+            errorMessage.disabled = false;
+
+            resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+            problemCause.placeholder = "Descreva o que ocasionou o erro ou situação"
         }
+        else{
+
+        if (!problemCheckbox.checked && !doubtCheckbox.checked) {
+            createNotif("Selecione um tipo de chamado", "error");
+            return;
+        }
+        else if (!problemCause.value) {
+            createNotif("Descreva a causa do problema", "error")
+            return;
+        }
+        else if (!resolution.innerText) {
+            createNotif("Informe a resolução do suporte", "error")
+            return;
+        }
+        else if (humorSelection.value === 'Selecione') {
+            createNotif("Selecione o humor do cliente", "error")
+            return;
+        }
+        else if (!upsellSituationNo.checked && !upsellSituationYes.checked) {
+            createNotif("Seleciona a situação do upsell", "error")
+            return;
+        }
+        else if (upsellSituationYes.checked && upsellDescription.value === "") {
+            createNotif("Descreva a situação do upsell", "error")
+            return;
+        }
+        else if (upsellSituationNo.checked && upsellDescription.value === "") {
+            createNotif("Descreva a situação do upsell", "error")
+            return;
+        }
+        else {
+
+            // Capturar os valores e adicionar ao array
+            addText('TIPO DE CHAMADO', 'Problema');
+            addText('NÚMERO DO DOCUMENTO', docNumber.value);
+            addText('MENSAGEM DE ERRO', errorMessage.value);
+            addText('CAUSA DO PROBLEMA', problemCause.value);
+            addText('RESOLUÇÃO', resolution.innerText);
+            addText('FEEDBACK DO CLIENTE', clientFeedback.value);
+            addText('HUMOR DO CLIENTE', humorSelection.value);
+            addText('UPSELL', document.querySelector('#upsellSituationYes').checked ? 'Sim' : (document.querySelector('#upsellSituationNo').checked ? 'Não' : ''));
+            addText('DESCRIÇÃO UPSELL', upsellDescription.innerText);
+            addText('MENSAGENS OU PRINT DE ERROS', document.querySelector('#errorPrintYes').checked ? 'Sim' : (document.querySelector('#errorPrintNo').checked ? 'Não' : ''));
+
+            // Unir todos os textos em uma string com quebras de linha
+            let finalText = textsToCopy.join('');
+            navigator.clipboard.writeText(finalText);
+            createNotif("Texto Copiado!", "success");
+
+            docNumber.value = '';
+            errorMessage.value = '';
+            problemCause.value = '';
+            resolution.innerText = '';
+            clientFeedback.value = '';
+            upsellDescription.innerText = '';
+
+            humorSelection.value = 'Selecione';
+
+            problemCheckbox.checked = false;
+            upsellSituationYes.checked = false;
+            upsellSituationNo.checked = false;
+            errorPrintYes.checked = false;
+            errorPrintNo.checked = false;
+
+            resolution.setAttribute("data-placeholder", "Descreva a resolução do problema");
+            problemCause.placeholder = "Descreva o que ocasionou o erro ou situação"
+        }
+    }
     })
 
-    function createNotif(message, type){
+    function createNotif(message, type) {
         let notifSpan = document.getElementById("notifSpan");
         const notifsBox = document.getElementById("notifsBox");
 
-        if(!notifSpan){
+        if (!notifSpan) {
             notifSpan = document.createElement("span");
             notifSpan.id = "notifSpan";
             notifSpan.innerHTML = message;
@@ -110,15 +296,15 @@ document.addEventListener("DOMContentLoaded", () => {
             notifSpan.classList.add(type);
             notifsBox.appendChild(notifSpan);
         }
-        
-        setTimeout(() =>{
+
+        setTimeout(() => {
             notifsBox.classList.replace("slideIn", "slideOut");
-        },2700)
-        
-        setTimeout( () =>{
+        }, 2700)
+
+        setTimeout(() => {
             notifsBox.classList.remove("slideOut");
             notifSpan.classList.remove(type);
-            if(notifSpan.parentNode) notifsBox.removeChild(notifSpan);
+            if (notifSpan.parentNode) notifsBox.removeChild(notifSpan);
         }, 3000)
     }
 })
